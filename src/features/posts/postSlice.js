6453e2dd4900dcd23da7bba5bee54a771a687ceb
SCARRAIDER <rsahanediriweera@gmail.com>
2023-03-29 11:44:49 +0530

    fixed the emojis

--- src/features/posts/postSlice.js
+++ src/features/posts/postSlice.js
@@ -89,11 +89,11 @@ const postsSlice = createSlice({
                     post=> {
                         post.date = sub(new Date(),{minutes:min++}).toISOString();
                         post.reactions = {
-                            thumbsUp:0,
-                            hooray:0,
-                            heart:0,
-                            rocket:0,
-                            eyes:0,
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
                         }
                         return post;
                     }
