f323208155c93b95702192b5226610ed0b725a05
SCARRAIDER <rsahanediriweera@gmail.com>
2023-04-24 13:19:22 +0530

    completing

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,27 +1,38 @@
-import { useSelector } from "react-redux";
-import { selectPostById } from "./postSlice";
+import { useSelector } from 'react-redux'
+import { selectPostById } from './postsSlice'
 
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import ReactionButtons from './ReactionButtons';
 
-import React from 'react'
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const SinglePostPage = () => {
+    const { postId } = useParams()
 
-    const post = useSelector((state)=>selectPostById(state,postId));
+    const post = useSelector((state) => selectPostById(state, Number(postId)))
 
-    if(!post){
-        return(
+    if (!post) {
+        return (
             <section>
-                <h2>Post Not Found</h2>
+                <h2>Post not found!</h2>
             </section>
         )
     }
 
-  return (
-    <div>SinglePostPage</div>
-  )
+    return (
+        <article>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <p className="postCredit">
+                <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
+            <ReactionButtons post={post} />
+        </article>
+    )
 }
 
 export default SinglePostPage
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import {store} from './app/store';
 import { Provider } from 'react-redux';
 import { fetchUsers } from './features/users/usersSlice';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 store.dispatch(fetchUsers());
 
@@ -12,7 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element = {<App/>}/>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
